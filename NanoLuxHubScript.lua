83452c6637deeb59af1fa65aafce9e11daa1bc58
NanoLux Company <mn-msihek@mail.ru>
2025-02-13 19:23:07 +0500

    Update NanoLuxHubScript.lua

--- NanoLuxHubScript.lua
+++ NanoLuxHubScript.lua
@@ -1,15 +1,27 @@
 local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/NanoLuxCompany/NanoLuxHub/refs/heads/main/Gamelist.lua"))()
 
+if not Games then
+    game.Players.LocalPlayer:Kick("Failed to load game list. Please try again later.")
+    return
+end
+
+local foundScript = false
+
 for PlaceID, Execute in pairs(Games) do
     if PlaceID == game.PlaceId then
         game.StarterGui:SetCore("SendNotification", {
-        Title = "NanoLux Script Hub";
-        Text = "The script is loading, please wait.";
-        Icon = "";
-        Duration = "3";
-        Button1 = "Okay";})
+            Title = "NanoLux Script Hub",
+            Text = "The script is loading, please wait.",
+            Icon = "",
+            Duration = 3,
+            Button1 = "Okay"
+        })
         loadstring(game:HttpGet(Execute))()
-    --else
-        --game.Players.LocalPlayer:Kick("There is no script added to this game yet. Please wait. Best regards, NanoLuxHub.") end
+        foundScript = true
+        break
     end
+end
+
+if not foundScript then
+    game.Players.LocalPlayer:Kick("There is no script added to this game yet. Please wait. Best regards, NanoLuxHub.")
 end
