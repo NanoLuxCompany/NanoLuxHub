430e81614d361cec3adb3237d58a0a1f26642838
NanoLux Company <mn-msihek@mail.ru>
2025-11-25 23:47:36 +0500

    Update NanoLuxScriptLib.lua

--- scripts/UiLib/NanoLuxScriptLib.lua
+++ scripts/UiLib/NanoLuxScriptLib.lua
@@ -167,7 +167,7 @@ function Library:Create(xHubName,xGameName)
     TabHolder.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
     TabHolder.BackgroundTransparency = 1.000
     TabHolder.BorderSizePixel = 0
-    TabHolder.Position = UDim2.new(0.244827583, 0, 0.054, 0) -- Moved lower from 0.024
+    TabHolder.Position = UDim2.new(0.20, 0, 0.054, 0) -- Moved lower from 0.024
     TabHolder.Size = UDim2.new(0, 424, 0, 490) -- Increased height
 
     Tabs.Name = "Tabs"
